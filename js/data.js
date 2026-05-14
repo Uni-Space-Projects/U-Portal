5088fff531d3aaef64b1c50b39f96800660a0f13
Franco Nuzzo <franconuzzo2007@gmail.com>
2026-05-13 22:58:47 -0400

    fixed usless data

--- js/data.js
+++
@@ -1,7 +0,0 @@
-// data.js - Datos estáticos 
-const transacciones = [
-    { id: "TX100", fecha: "10/05/2026", tipo: "entrada", descripcion: "Depósito nómina", monto: 500.00 },
-    { id: "TX101", fecha: "11/05/2026", tipo: "salida", descripcion: "Pago móvil", monto: -50.00 },
-    { id: "TX102", fecha: "12/05/2026", tipo: "entrada", descripcion: "Transferencia recibida", monto: 120.00 },
-    { id: "TX103", fecha: "13/05/2026", tipo: "salida", descripcion: "Transferencia enviada", monto: -200.00 }
-];
